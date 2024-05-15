98009a21e90242de6738f0bca4c5c0377bcfd5b1
Jobdizon <jobdizon20@yahoo.com>
2024-05-15 12:50:22 -0800

    Refreshed styling and added DM Sans font

---
+++ my-portfolio/.svelte-kit/output/client/_app/immutable/entry/start.CBRLkPmi.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Cbv6ezjd.js";export{t as start};
---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,34 @@
+<body>
+    <section class="main">
+        <div class="main-container">
+            <div class="column-left">
+                <h1>Job Daniel Dizon</h1>
+                <p>
+                    Computer Systems Engineering student
+                    at the University of Alaska Anchorage.
+                </p>
+            </div>
+        </div>
+    </section>
+    <article class="description">
+        <div class="about">
+            <h2>About</h2>
+            <p>
+                insert paragraph here
+            </p>
+        </div>
+        <div class="experience">
+            <h2>Experience</h2>
+            <p>
+                insert paragraph here
+            </p>
+        </div>
+    </article>
+</body>
+
+<style>
+    @import './mainstyle.css';
+</style>
+
+
+
