db0d40bb76f2e2a7486d2aa33fce1d7bb436e77a
Jobdizon <jobdizon20@yahoo.com>
2024-05-15 12:56:13 -0800

    removed my-portfolio dir

--- my-portfolio/.svelte-kit/output/client/_app/immutable/entry/start.CBRLkPmi.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Cbv6ezjd.js";export{t as start};
--- my-portfolio/.svelte-kit/output/server/entries/pages/_page.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<h1 data-svelte-h="svelte-1t7tj1k">Job Daniel Dizon</h1> <p data-svelte-h="svelte-18n7gae">A second-year Computer Systems Engineering student
-    at the University of Alaska Anchorage</p>`;
-});
-export {
-  Page as default
-};
