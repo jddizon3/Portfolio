692568cdadb07c2f3ca83a2d55d980414c690090
Jobdizon <jobdizon20@yahoo.com>
2024-05-17 13:02:02 -0800

    Added links and content for about and experience

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <body>
-    <section class="main">
+    <div class="main">
         <div class="main-container">
             <div class="column-left">
                 <h1>Job Daniel Dizon</h1>
@@ -8,22 +8,39 @@
                     at the University of Alaska Anchorage.
                 </p>
             </div>
+            <div class="links">
+                <ul>
+                    <a href="https://github.com/jddizon3" class="github"><img src="./images/icons8-github-384.png" alt="Github"></a>
+                    <a href="https://linkedin.com/in/jddizon" class="linkedin"><img src="./images/icons8-linkedin-384.png" alt=Linkedin></a>
+                    <a href="./Job Daniel Dizon (2024).pdf" class="resume"><img src="./images/icons8-resume-100.png" alt=Resume></a>
+                </ul>
+            </div>
         </div>
-    </section>
-    <section class="description">
+    </div>
+    <div class="description">
         <div class="about">
             <h2>About</h2>
             <p>
-                insert paragraph here
+                I have always been intrigued by the inner workings of computers, 
+                fascinated by how these machines perform complex tasks. 
+                This curiosity led me to pursue a deeper understanding of computer technology, 
+                and when I entered college, I eagerly took on the challenge of studying Computer Systems Engineering. 
+                Now, in my third year, my initial curiosity has deeper appreciation for the field. 
+                Through various challenging projects and courses, I have developed a solid foundation of skills and knowledge, 
+                preparing me for the evolving technological industry and reinforcing my commitment to a 
+                career in computer systems engineering. 
+            </p>
+            <p>
+                Outside the computer, you'll find me at basketball court, reading, playing with my dog and playing chess.
             </p>
         </div>
         <div class="experience">
             <h2>Experience</h2>
             <p>
-                insert paragraph here
+                Seeking an internship.
             </p>
         </div>
-    </section>
+    </div>
 </body>
 
 <style>
